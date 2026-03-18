ca83c941e115c38013cce25b0f0110187116078d
cynthia <cynthiagutierrezvargas@gmail.com>
2026-03-18 00:07:00 -0300

    proyecto inicial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: 'https://joseriquelme1773800460000.0900200.misitiohostgator.com', // Cámbialo por tu dominio real
+  base: '/',
+});
