43e13a5fd003f82de390fd95c21351d734ca250b
cynthia <cynthiagutierrezvargas@gmail.com>
2026-03-23 17:30:29 -0300

    mejoras SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -8,6 +8,10 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://joseriquelme1773800460000.0900200.misitiohostgator.com', // Cámbialo por tu dominio real
-  base: '/',
+  site: 'https://https://yourbusinessweb.com', // Cámbialo por tu dominio real
+  integrations: [sitemap()],
+  redirects: {
+    '/preguntas': '/faq', // Si alguien entra a /preguntas, se va a /faq
+    '/ayuda': '/faq'
+  }
 });
